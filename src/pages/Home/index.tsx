a5886b1b240bd005af783d462b6b48c7117f6283
Marcus Sillion <marcus.costa@sillion.com.br>
2025-02-04 13:31:34 -0300

    fix: ajustando altura

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   };
 
   return (
-    <section className="my-8 h-screen">
+    <section className="my-8 h-full">
       <main className="custom-container">
         <div className="flex flex-wrap gap-10 md:items-center md:justify-between">
           <div
